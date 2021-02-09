3d8ad276b61befe7e0394c43c7123ccd3fc899fc
khanhnoi <nqkhanh1998@gmail.com>
2021-02-09 23:14:50 +0700

    add videoSteam

--- public/script.js
+++ public/script.js
@@ -1,4 +1,24 @@
-navigator.mediaDevices.getUserMedia({
-  video: true,
-  audio: true,
-});
+const videoGrid = document.getElementById("video-grid");
+console.log(videoGrid);
+const myVideoElm = document.createElement("video");
+
+// myVideoElm.muted = true;
+
+let myVideoStream;
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then(function (stream) {
+    myVideoStream = stream;
+    addVideoStream(myVideoElm, stream);
+  });
+
+const addVideoStream = (videoElm, stream) => {
+  videoElm.srcObject = stream;
+  videoElm.addEventListener("loadedmetadata", () => {
+    videoElm.play();
+  });
+  videoGrid.append(videoElm);
+};
