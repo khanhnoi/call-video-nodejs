68309b6a56b8d39c85102f0ab8e4dc70d638edac
khanhnoi <nqkhanh1998@gmail.com>
2021-02-11 00:11:09 +0700

    add peerJs and setup

--- public/script.js
+++ public/script.js
@@ -1,30 +1,89 @@
-const socket = io();
-
 const videoGrid = document.getElementById("video-grid");
-console.log(videoGrid);
-const myVideoElm = document.createElement("video");
+const btnCam = document.getElementById("onCam");
+const socket = io("/");
+const peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "5000",
+});
+let myStreamVideoSave;
 
-myVideoElm.muted = true;
-
-let myVideoStream;
-navigator.mediaDevices
-  .getUserMedia({
-    video: true,
-    audio: true,
-  })
-  .then(function (stream) {
-    myVideoStream = stream;
-    addVideoStream(myVideoElm, stream);
-  });
-
-socket.emit("join-room", () => {
-  console.log("Joined Room Succues !!");
+peer.on("open", function (peerId) {
+  console.log("My peer ID is: " + peerId);
+  const titleTdElm = document.getElementById("peerId");
+  titleTdElm.append(peerId);
+  socket.emit("join-room", { roomId: roomId, peerId: peerId });
 });
 
-const addVideoStream = (videoElm, stream) => {
-  videoElm.srcObject = stream;
-  videoElm.addEventListener("loadedmetadata", () => {
-    videoElm.play();
+// myVideoElm.muted = true;
+// let myVideoStream;
+// console.log(videoGrid);
+
+OpenStream().then(function (myVideoStream) {
+  myStreamVideoSave = myVideoStream;
+  playVideoStream("myVideo", myVideoStream);
+
+  socket.on("user-connected", (data) => {
+    //anothorUserId is peerId
+    const { anotherUserId } = data;
+    console.log("New User Joind: " + anotherUserId);
+    //send
+    const call = peer.call(anotherUserId, myVideoStream);
+    //recevice
+    call.on("stream", function (remoteStreamVideo) {
+      // Show stream in some video/canvas element.
+      console.log("remote of send");
+      playVideoStream("remoteVideo", remoteStreamVideo);
+    });
   });
-  videoGrid.append(videoElm);
+});
+
+peer.on("call", function (call) {
+  OpenStream().then((myVideoStream) => {
+    call.answer(myVideoStream); // Answer the call with an A/V stream.
+    call.on("stream", function (remoteStreamVideo) {
+      // Show stream in some video/canvas element.
+      console.log("remote of receive");
+      playVideoStream("remoteVideo", remoteStreamVideo);
+    });
+  });
+});
+
+//build functions
+function OpenStream() {
+  return navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  });
+}
+
+const playVideoStream = (videoElmId, stream) => {
+  const remoteVideoElm = document.getElementById(videoElmId);
+  if (!remoteVideoElm) {
+    const newVideoElm = document.createElement("video");
+    newVideoElm.setAttribute("id", videoElmId);
+    newVideoElm.muted = true;
+    newVideoElm.srcObject = stream;
+    newVideoElm.addEventListener("loadedmetadata", () => {
+      newVideoElm.play();
+    });
+    videoGrid.append(newVideoElm);
+  } else {
+    remoteVideoElm.srcObject = stream;
+    remoteVideoElm.addEventListener("loadedmetadata", () => {
+      remoteVideoElm.play();
+    });
+  }
+};
+
+const connectToNewServer = (anothorUserId, stream) => {
+  //   console.log("New user has Id " + AnothorUserId);
+  // Call a peer, providing our mediaStream
+  //Phia nguoi Goi
+  const call = peer.call(anothorUserId, stream);
+  call.on("stream", (remoteVideoStream) => {
+    // addVideoStream(newVideoElm, userVideoStream);
+    console.log("Remote Video 1");
+    playVideoStream2("remoteVideo", remoteVideoStream);
+  });
 };
--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const server = http.Server(app);
+const io = require("socket.io")(server);
 require("dotenv").config();
-const io = require("socket.io")(app);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
 const PORT = process.env.PORT || 5000;
 const roomRouter = require("./router/room");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use("/peerjs", peerServer);
 
 app.use("/", roomRouter);
 
-io.on("connection", (socket) => {
-  console.log("A user connected !!");
-  socket.on("join-room", () => {
-    console.log("A User joined Room");
-  });
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
+server.listen(PORT, function () {
+  console.log(`Sever is running at ${PORT}`);
 });
 
-app.listen(PORT, function () {
-  console.log(`Sever is running at ${PORT}`);
+io.on("connection", (socket) => {
+  socket.on("join-room", (data) => {
+    const { roomId, peerId } = data;
+    console.log(`+ ${peerId} JOINED ROOM ${roomId}`);
+    socket.join(roomId);
+    socket
+      .to(roomId)
+      .broadcast.emit("user-connected", { anotherUserId: peerId });
+  });
+  socket.on("disconnect", () => {
+    console.log("+ user disconnected: ");
+    // console.log(socket.id);
+  });
 });
